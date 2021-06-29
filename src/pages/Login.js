e5eb56333bb79b60df7ca0745e9fee647b352e9b
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-29 05:39:22 +0100

    collecting aut form data

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [signInData, setSignInData] = useState({email: '', password: ''})
+    const handleChange = e => {
+        let {name, value} = e.target
+        setSignInData({...signInData, [name]: value})
+    }
+    const submitForm = e => {
+        e.preventDefault();
+        console.log(signInData)
+    }
     return (
         <div>
-            <form className="container">
+            <form className="container" onSubmit={submitForm}>
               <div className="form-group">
                   <label htmlFor="" className="email">Email</label>
-                  <input type="email" className="form-control" />
+                  <input name="email" onChange={handleChange} type="email" className="form-control" />
               </div>
               <div className="form-group">
                   <label htmlFor="" className="password">Password</label>
-                  <input type="password" className="form-control" />
+                  <input name="password" onChange={handleChange} type="password" className="form-control" />
               </div>
               <button className="btn btn-primary btn-block">Login</button>
             </form>  
