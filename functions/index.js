e39bc3280c057d45736bf24f6c6e4c46292ed1bb
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-29 07:40:13 +0100

    cloud functions done, now consuminggit add .!

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const serviceAccount = require("testproject2-73370-firebase-adminsdk-l6v36-92b28dc61b.json");
+const serviceAccount = require("./testproject2-73370-firebase-adminsdk-l6v36-92b28dc61b.json");
 const bcrypt = require("bcrypt")
 
 
@@ -11,85 +11,97 @@ admin.initializeApp({
 
 exports.register = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    const password = await bcrypt.hash(req.body.password, await bcrypt.genSalt())
-    admin
-    .auth()
-    .createUser({
-      email: req.body.email,
-      password: password,
-      isAdmin: false,
-    })
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log('Successfully created new user:', userRecord.uid);
-      res.status(201).json({id: userRecord.uid, ...userRecord.toJSON()})
-    })
-    .catch((error) => {
-      console.log('Error creating new user:', error);
-      res.status(400).json({
-          message: error.message,
-      })
-    });
+    if (req.method === 'POST') {
+        const password = await bcrypt.hash(req.body.password, await bcrypt.genSalt())
+        admin
+        .auth()
+        .createUser({
+            email: req.body.email,
+            password: password,
+            isAdmin: false,
+        })
+        .then((userRecord) => {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log('Successfully created new user:', userRecord.uid);
+            res.status(201).json({id: userRecord.uid, ...userRecord.toJSON()})
+        })
+        .catch((error) => {
+            console.log('Error creating new user:', error);
+            res.status(400).json({
+                message: error.message,
+            })
+        });
+    } else {
+        res.status(405).status({message: 'Method not allowed'})
+    }
 });
 
 exports.login = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    admin
-    .auth()
-    .getUserByEmail(
-        req.body.email
-    )
-    .then((userRecord) => {
-        // See the UserRecord reference doc for the contents of userRecord.
-        console.log('Successfully created new user:', userRecord.uid);
-        if (await bcrypt.compare(req.body.password, userRecord.passwordHash)) {
-            res.status(200).json({id: userRecord.uid, ...userRecord.toJSON()})
-        } else {
-            res.status(401).json({message: 'Invalid email or password'})
-        }
-    })
-    .catch((error) => {
-      console.log('Error creating new user:', error);
-      res.status(400).json({
-          message: error.message,
-      })
-    });
+    if (req.method === 'POST') {
+        admin
+        .auth()
+        .getUserByEmail(
+            req.body.email
+        )
+        .then(async (userRecord) => {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log('Successfully logged in:', userRecord.uid);
+            if (await bcrypt.compare(req.body.password, userRecord.passwordHash)) {
+                res.status(200).json({id: userRecord.uid, ...userRecord.toJSON()})
+            } else {
+                res.status(401).json({message: 'Invalid email or password'})
+            }
+        })
+        .catch((error) => {
+            console.log('Error logging in:', error);
+            res.status(400).json({
+                message: error.message,
+            })
+        });
+    } else {
+        res.status(405).status({message: 'Method not allowed'})
+    }
 });
 
 exports.create_data = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    admin
-    .auth()
-    .getUser(
-        req.body.userId
-    )
-    .then((userRecord) => {
-        admin.firestore().collection('data').add({
-            user: {
-                id: userRecord.uid,
-                email: userRecord.email
-            },
-            username: req.body.username,
-            text: req.body.text
-        }).then(data => {
-            res.status(201).json({
-                message: 'Successfully created data',
-                data: {
-                    ...data
-                }
+    if (req.method ===  'POST') {
+        admin
+        .auth()
+        .getUser(
+            req.body.userId
+        )
+        .then((userRecord) => {
+            admin.firestore().collection('data').add({
+                user: {
+                    id: userRecord.uid,
+                    email: userRecord.email
+                },
+                username: req.body.username,
+                text: req.body.text
+            }).then(data => {
+                res.status(201).json({
+                    message: 'Successfully created data',
+                    data: {
+                        ...data
+                    }
+                })
             })
         })
-    })
-    .catch((error) => {
-      console.log('Error creating new user:', error);
-      res.status(400).json({
-          message: error.message,
-      })
-    });
+        .catch((error) => {
+            console.log('Error creating new user:', error);
+            res.status(400).json({
+                message: error.message,
+            })
+        });
+    } else {
+        res.status(405).status({message: 'Method not allowed'})
+    }
 });
 
 
-exports.add_log = functions.database('/data/{dataId}').onCreate((snapshot, ctx) => {
+exports.add_log = functions.firestore.document('/data/{dataId}').onCreate((snapshot, ctx) => {
     let data = snapshot.val()
     admin.firestore().collection('log').add({
         email: data.user.email,
--- src/firebaseUtils/index.js
+++ src/firebaseUtils/index.js
@@ -1,17 +1,27 @@
-import firebase from 'firebase'
+// import firebase from 'firebase'
 
-let firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    databaseUrl: process.env.REACT_APP_databaseUrl
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// let firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId,
+//     databaseUrl: process.env.REACT_APP_databaseUrl
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth()
+// export const auth = firebase.auth()
 
-export const firestore = firebase.firestore()
+// export const firestore = firebase.firestore()
+
+import axios from 'axios'
+
+const signup = async (email, password) => {
+    try {
+        let res = await axios.get()
+    } catch (error) {
+        
+    }
+}
