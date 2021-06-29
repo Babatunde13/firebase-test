cd5b914622205078348f07d642d34e36115acaf3
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-29 05:41:53 +0100

    form data handling for auth

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState }from 'react'
 
 const Form = () => {
+    const [formData, setFormData] = useState({username: '', password: ''})
+    const handleChange = e => {
+        let {name, value} = e.target
+        setFormData({...formData, [name]: value})
+    }
+    const submitForm = e => {
+        e.preventDefault();
+        console.log(formData)
+    }
     return (
         <div>
-            <form className="container">
+            <form className="container" onSubmit={submitForm}>
               <div className="form-group">
                   <label htmlFor="" className="email">username</label>
-                  <input type="email" className="form-control" />
+                  <input name="username" onChange={handleChange}type="text" className="form-control" />
               </div>
               <div className="form-group">
                   <label htmlFor="" className="password">text</label>
-                  <input type="password" className="form-control" />
+                  <input name="password" onChange={handleChange}type="password" className="form-control" />
               </div>
               <button className="btn btn-primary btn-block">Submit</button>
             </form>  
